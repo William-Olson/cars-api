46b63874f84f26c941177b3125b0be36526a5b95
Will Olson <willko747@gmail.com>
2019-12-19 00:20:36 -0600

    adding comments to esSettings

--- src/services/elasticsearch/esSettings.ts
+++ src/services/elasticsearch/esSettings.ts
@@ -1,11 +1,17 @@
+/*
 
+  Defines a custom analyzer and filter.
+  Text is converted to lowercase and broken up into
+  tokens at n-gram length.
+
+*/
 export const elasticSearchSettings = {
   number_of_shards: 1,
   analysis: {
     filter: {
       car_filter: {
         type: 'edge_ngram',
-        min_gram: 1,
+        min_gram: 2,
         max_gram: 20
       }
     },
@@ -22,6 +28,11 @@ export const elasticSearchSettings = {
   }
 };
 
+/*
+
+  Defines Elasticsearch field types and analyzers for cars.
+
+*/
 export const elasticSearchMappings = {
   default: {
     properties: {
